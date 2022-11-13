2e8553aabc7040b25bc17490a03351d4a1befcd1
Anton <shut86@inbox.ru>
2022-11-13 22:45:19 +0300

    Modify Task9

--- 009/Program.cs
+++ 009/Program.cs
@@ -1,9 +1,9 @@
 ﻿//9. Вывести на экран четные числа от 1 до N
 
 int n=Convert.ToInt32(Console.ReadLine());
-int i=1;
+int i=2;
 while(i<=n)
 {
-    System.Console.Write($"{i+1} ");
+    System.Console.Write($"{i} ");
     i=i+2;
 }
