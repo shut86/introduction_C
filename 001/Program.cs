23be745a17d4d655c0f1d31d492e640f262189b4
Anton <shut86@inbox.ru>
2022-11-13 21:41:04 +0300

    Task 1

--- 001/Program.cs
+++ 001/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//1. С клавиатуры вводится целое число. Вывести квадрат числа (блок-схема)
+
+int a;
+System.Console.Write("Введите чило:");
+string s=Console.ReadLine();
+a=Convert.ToInt32(s);
+int b=a*a;
+System.Console.WriteLine(b);
+
+
