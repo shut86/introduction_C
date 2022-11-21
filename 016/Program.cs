ea7581b6e063e566e349dcc86cfb8bc0da28058f
Anton <shut86@inbox.ru>
2022-11-21 18:25:14 +0300

    Add task 16,17,18,19,23

---
+++ 016/Program.cs
@@ -0,0 +1,4 @@
+﻿//16. Дано число. Проверить кратно ли оно 7 и 23
+
+int n=Convert.ToInt32(Console.ReadLine());
+if (n%7==0 && n%23==0) System.Console.WriteLine("YES"); else System.Console.WriteLine("NO");
---
+++ 017/Program.cs
@@ -0,0 +1,12 @@
+﻿//17. Дано число обозначающее день недели. Выяснить является номер дня недели выходным
+
+System.Console.WriteLine("Введите число от 1 до 7: ");
+int a=Convert.ToInt32(Console.ReadLine());
+if (a == 6 || a == 7) 
+{
+    System.Console.WriteLine("Сегодня выходной");
+}
+else 
+{
+    System.Console.WriteLine("Сегодня рабочий день");
+}
