c82a63a4061f1106616c5dea8bc38dc20040d2da
Anton <shut86@inbox.ru>
2022-11-24 23:01:20 +0300

    Lesson 3

---
+++ 028/Program.cs
@@ -0,0 +1,13 @@
+﻿//28. Определить количество цифр в числе. Сделать подпрограмму.
+
+int N=Convert.ToInt32(Console.ReadLine());
+N=Math.Abs(N);
+int count=0;
+if (N==0) count=1;
+while (N>0)
+{
+    N=N/10;
+    count++;
+}
+
+System.Console.WriteLine(count);
---
+++ 031/Program.cs
@@ -0,0 +1,11 @@
+﻿//31. Сгенерировать 10 случайных чисел. Показать кубы чисел, которые заканчиваются на четную цифру
+
+Random random=new Random(10);
+for(int i=0;i<10;i++)
+{
+    int a=random.Next(1,100);
+    int b=a;
+    if (b%2==0)
+    System.Console.WriteLine($"{a}, {b}");
+}
+    
